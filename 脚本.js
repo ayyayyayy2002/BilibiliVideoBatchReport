03542c8d0a8f8ac8c0649f33ab0536e50b99911a
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-10-15 16:11:28 +0800

    Update 脚本.js

--- 脚本.js
+++ 脚本.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         BiliBili稿件批量举报
+// @name         BiliBili视频批量举报
 // @namespace    https://github.com/ayyayyayy2002/BilibiliVideoBatchReport
 // @version      0.1.7
 // @description  BiliBili屎太多，黑名单不够用了，我很痛苦，于是写了这个脚本尝试将痛苦转移到发布视频的人身上，我准备了三个举报理由，点击按钮即可切换
@@ -10,6 +10,7 @@
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
 // @exclude      https://space.bilibili.com/*/dynamic
+// @exclude      https://space.bilibili.com/*/article
 // @grant        GM_registerMenuCommand
 // @grant        GM_unregisterMenuCommand
 // @connect      api.bilibili.com
